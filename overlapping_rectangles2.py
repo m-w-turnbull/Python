2efab6f073da977d305da3aad846010ee1d2e0e9
m-w-turnbull <michael.w.turnbull@gmail.com>
2019-11-21 11:02:38 -0800

    modularized

--- overlapping_rectangles2.py
+++ overlapping_rectangles2.py
@@ -2,40 +2,42 @@ coord_set1 = [[-3, -2], [1, 2]]
 coord_set2 = [[-5, 1], [-2, 5]]
 
 def find_maxes(coord1, coord2):
-    x1 = coord1[0]
-    x2 = coord2[0]
-    if x1 >= x2:
-        maxx = x1
-    else:
-        maxx = x2
-    y1 = coord1[1]
-    y2 = coord2[1]
-    if y1 >= y2:
-        maxy = y1
-    else:
-        maxy = y2
+    maxx = []
+    maxx.append(coord1[0])
+    maxx.append(coord2[0])
+    maxx = max(maxx)
+    maxy = []
+    maxy.append(coord1[1])
+    maxy.append(coord2[1])
+    maxy = max(maxy)
     return(maxx, maxy)
 
 def find_mins(coord1, coord2):
-    x1 = coord1[0]
-    x2 = coord2[0]
-    y1 = coord1[1]
-    y2 = coord2[1]
-    if x1 <= x2:
-        minx = x1
-    else:
-        minx = x2
-    if y1 <= y2:
-        miny = y1
-    else:
-        miny = y2
+    minx = []
+    miny = []
+    minx.append(coord1[0])
+    minx.append(coord2[0])
+    miny.append(coord1[1])
+    miny.append(coord2[1])
+    minx = min(minx)
+    miny = min(miny)
     return(minx, miny)
 
+def find_the_overlap(coordset1, coordset2):
+    overlap = []
+    overlap.append(find_maxes(coordset1[0], coordset2[0]))
+    overlap.append(find_mins(coordset1[1], coordset2[1]))
+    return overlap
+
+def find_the_area_of_overlap(overlap_coords):
+    overlapx = overlap[0][0] - overlap[1][0]
+    overlapy = overlap[0][1] - overlap[1][1]
+    area_of_overlap = overlapx * overlapy
+    return area_of_overlap
 
 
-overlap = []
-overlap.append(find_maxes(coord_set1[0], coord_set2[0]))
-overlap.append(find_mins(coord_set1[1], coord_set2[1]))
-print(overlap)
 
 
+overlap = find_the_overlap(coord_set1, coord_set2)
+print("The area of the overlap is:", find_the_area_of_overlap(overlap))
+print("The coordinates of the overlap are:", overlap)
