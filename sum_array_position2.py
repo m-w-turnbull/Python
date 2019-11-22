ca936719e2550842ca0f1d6618f96a1ff8c295de
m-w-turnbull <michael.w.turnbull@gmail.com>
2019-11-21 23:24:32 -0800

    optimize and function

--- sum_array_position2.py
+++ sum_array_position2.py
@@ -1,17 +1,20 @@
-master = [1, 2, 4, 5, 6, 27, 92, 14, 28, 75]
+input_array = [1, 2, 4, 5, 6, 27, 92, 14, 28, 75]
 target = 42
 
-x = 0
-y = 0
 
-while y <= len(master) and x <= len(master):
-    if sum(master[x:y]) == target:
-        print(master[x:y])
-        print("Position: ", x + 1)
-    elif sum(master[x:y + 1]) > target:
-        x += 1
-        y = x
-    y += 1
-    if y == len(master) + 1:
-        x += 1
-        y = x
+def sum_array_pos(target, input_array):
+    x = 0
+    y = 0
+    while y <= len(input_array) and x <= len(input_array):
+        if sum(input_array[x:y]) == target:
+            print(input_array[x:y])
+            print("Position: ", x + 1)
+        elif sum(input_array[x:y + 1]) > target:
+            x += 1
+            y = x
+        y += 1
+        if y == len(input_array) + 1:
+            x += 1
+            y = x
+
+sum_array_pos(target, input_array)
